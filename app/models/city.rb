845c1feecf9b739d4ac0301ccd8a8b9afe471c84
Felipe Rojas <frodelab@github.com>
2019-02-09 16:30:40 -0300

    condition in case api exceed limit

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,11 +3,18 @@ class City < ApplicationRecord
   # coords_array is an array of cities with lat and lon
   def self.time_weather_by_coords(lat, lon)
     data = ForecastIO.forecast(lat, lon)
-    temperature = Temperature.from_fahrenheit(data.currently.temperature)
-    datetime = data.currently.time
-    timezone = data.timezone
-    date = Time.at(datetime).in_time_zone(timezone).strftime('%a %d %b')
-    time = Time.at(datetime).in_time_zone(timezone).strftime('%H:%M')
-    [date, time, temperature.celsius.round(0)]
+    if data.present?
+      temperature_f = Temperature.from_fahrenheit(data.currently.temperature)
+      temperature = temperature_f.celsius.round(0)
+      datetime = data.currently.time
+      timezone = data.timezone
+      date = Time.at(datetime).in_time_zone(timezone).strftime('%a %d %b')
+      time = Time.at(datetime).in_time_zone(timezone).strftime('%H:%M')
+    else
+      date = '-'
+      time = '--:--'
+      temperature = '-'
+    end
+    [date, time, temperature]
   end
 end
